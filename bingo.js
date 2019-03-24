fc2fba83c5109685c033696012f42bccb2a88c96
Tom de Bruin <tom@deadlyhifi.com>
2019-03-24 21:14:52 +0000

    More explanations

--- bingo.js
+++ bingo.js
@@ -94,71 +94,206 @@ const nicknames = {
   20: {
     saying: "One Score OR Getting Plenty",
     explanation:
-      'A reference to there being 20 units in one score. OR Plenty	Rhymes with "Twenty".'
+      'A reference to there being 20 units in one score. OR Rhymes with "Twenty".'
   },
   21: {
     saying: "Key of the Door.",
     explanation: "The traditional age of majority."
+  },
+  22: {
+    saying: "Two little swans.",
+    explanation:
+      'The numeral 22 resembles the profile of two ducks.[6] Response is often "quack, quack, quack".'
+  },
+  23: {
+    saying: "The Lord is My Shepherd.",
+    explanation: "The first words of Psalm 23 of the Old Testament"
+  },
+  24: {
+    saying: "Double dozen.",
+    explanation: "12 × 2 = 24"
+  },
+  25: {
+    saying: "Duck and dive.",
+    explanation: 'Rhymes with "(Twenty) Five"'
+  },
+  26: {
+    saying: "Two and six, half a crown. OR A to Z.",
+    explanation:
+      "Pre-decimalised currency in the UK. OR A reference to the fact that there are twenty-six letters in the alphabet."
+  },
+  27: {
+    saying: "Duck and a crutch.",
+    explanation:
+      "The number 2 looks like a duck (see '2') and the number 7 looks like a crutch."
+  },
+  28: {
+    saying: "In a state.",
+    explanation: 'Two and eight" is rhyming slang for "state".'
+  },
+  29: {
+    saying: "Rise and Shine.",
+    explanation: 'Rhymes with "(Twenty) Nine".'
+  },
+  30: {
+    saying: "Burlington Bertie. OR Dirty Gertie",
+    explanation:
+      'Reference to a music hall song of the same name composed in 1900, and a more famous parody (Burlington Bertie from Bow) written in 1915 specifically the line: "I’m Burlington Bertie I rise at ten-thirty". OR Common rhyme derived from the given name Gertrude, used as a nickname for the statue La Delivrance installed in North London in 1927. The usage was reinforced by Dirty Gertie from Bizerte, a bawdy song sung by Allied soldiers in North Africa during the Second World War.'
+  },
+  31: {
+    saying: "Get Up and Run.",
+    explanation: 'Rhymes with "(Thirty) One".'
+  },
+  32: {
+    saying: "Buckle My Shoe.",
+    explanation: 'Rhymes with "(Thirty) Two".'
+  },
+  33: {
+    saying: "All the threes. OR Fish, chips and peas.",
+    explanation: "All the threes. OR Rhymes."
+  },
+  34: {
+    saying: "Ask for More.",
+    explanation: 'Rhymes with "(Thirty) Four".'
+  },
+  35: {
+    saying: "Jump and Jive.",
+    explanation: "A dance step."
+  },
+  36: {
+    saying: "Triple dozen.",
+    explanation: "3 x 12 = 36."
+  },
+  37: {
+    saying: "More than Eleven.",
+    explanation: 'Rhymes with "(Thirty) Seven".'
+  },
+  38: {
+    saying: "Christmas cake.",
+    explanation: "Cockney rhyming slang."
+  },
+  39: {
+    saying: "Steps.",
+    explanation: "From the 39 Steps."
+  },
+  40: {
+    saying: "Life Begins.",
+    explanation: 'refers to the proverb "life begins at forty".'
+  },
+  41: {
+    saying: "Time for Fun.",
+    explanation: 'Rhymes with "(Fourty) One".'
+  },
+  42: {
+    saying: "Winnie the Pooh.",
+    explanation: 'Rhymes with "(Fourty) Two".'
+  },
+  43: {
+    saying: "Down on your knees.",
+    explanation:
+      "This was a phrase that was made popular during wartime by soldiers."
+  },
+  44: {
+    saying: "Droopy drawers.",
+    explanation: "Rhyme that refers to sagging trousers."
+  },
+  45: {
+    saying: "Halfway there.",
+    explanation: "Being halfway towards 90."
+  },
+  46: {
+    saying: "Up to tricks.",
+    explanation: 'Rhymes with "(Fourty) Six".'
+  },
+  47: {
+    saying: "Four and Seven.",
+    explanation: "The numbers of four and seven."
+  },
+  48: {
+    saying: "Four Dozen.",
+    explanation: "4 x 12 = 48."
+  },
+  49: {
+    saying: "P.C.",
+    explanation:
+      "Reference to 1946-53 show based on the adventures of an unconventional copper who coined the phrase “rise and shine”."
+  },
+  50: {
+    saying: "It's a bullseye! OR Snow White",
+    explanation:
+      "Referring to the darts score. OR Referring to 5 – 0, it’s off to work we go."
+  },
+  51: {
+    saying: "Tweak of the Thumb. OR I love my Mum.",
+    explanation: 'Rhymes with "(Fifty) One".'
+  },
+  52: {
+    saying: "Danny La Rue. OR Chicken vindaloo. OR Deck of Cards.",
+    explanation:
+      "A reference to drag entertainer Danny La Rue. OR Introduced by Butlins in 2003. OR Number of cards in a deck."
+  },
+  53: {
+    saying: "Here comes Herbie.",
+    explanation:
+      '53 is the racing number of Herbie the VW Beetle. Players may reply "beep beep"!.'
+  },
+  54: {
+    saying: "Man at the door.",
+    explanation: 'Rhymes with "(Fifty) Four".'
+  },
+  55: {
+    saying: "All the fives.",
+    explanation: "Five and five."
+  },
+  56: {
+    saying: "Shotts Bus.",
+    explanation:
+      "Refers to the former number of the bus from Glasgow to Shotts."
+  },
+  57: {
+    saying: "Heinz Varieties.",
+    explanation:
+      'Refers to "Heinz 57", the "57 Varieties" slogan of the H. J. Heinz Company.'
+  },
+  58: {
+    saying: "Make Them Wait.",
+    explanation: 'Rhymes with "(Fifty) Eight".'
+  },
+  59: {
+    saying: "The Brighton Line.",
+    explanation:
+      "Quote from The Importance of Being Earnest. Also, 59 was the starting 2 digits of all original Brighton telephone numbers."
+  },
+  60: {
+    saying: "Grandma's getting frisky.",
+    explanation: 'Pretty close to a rhyme with "sixty".'
   }
 
-  // 22	Two little swans	The numeral 22 resembles the profile of two ducks.[6] Response is often "quack, quack, quack".
-  // 23	The Lord is My Shepherd	The first words of Psalm 23 of the Old Testament
-  // 24	Double dozen	12 × 2 = 24. Refer to 12 above
-  // 25	Duck and dive	Rhymes with "(Twenty) Five"
-  // 26	Two and six, half a crown	Pre-decimalised currency in the UK. (See half crown)
-  // A to Z	A reference to the fact that there are twenty-six letters in the alphabet
-  // 27	Duck and a crutch.	The number 2 looks like a duck (see '2') and the number 7 looks like a crutch.
-  // 28	In a state.	"Two and eight" is rhyming slang for "state".
-  // 29	Rise and Shine	Rhymes with "(Twenty) Nine"
-  // 30	Burlington Bertie	Reference to a music hall song of the same name composed in 1900, and a more famous parody (Burlington Bertie from Bow) written in 1915 specifically the line: "I'm Burlington Bertie I rise at ten-thirty".
-  // Dirty Gertie[1]	Common rhyme derived from the given name Gertrude, used as a nickname for the statue La Delivrance installed in North London in 1927. The usage was reinforced by Dirty Gertie from Bizerte, a bawdy song sung by Allied soldiers in North Africa during the Second World War.[7]
-  // 31	Get Up and Run[1]	Rhymes with "(Thirty) One"
-  // 32	Buckle My Shoe[8]	Rhymes with "(Thirty) Two"
-  // 33	All the threes[4]
-  // Fish, chips and peas
-  // 34	Ask for More	Rhymes with "(Thirty) Four"
-  // 35	Jump and Jive[2]	A dance step
-  // 36	Triple dozen	3 x 12 = 36. Refer to 12 above
-  // 38	Christmas cake	Cockney rhyming slang
-  // 39	Steps	From the 39 Steps
-  // 40	Life Begins	refers to the proverb 'life begins at forty'
-  // 43	down on your knees	This was a phrase that was made popular during wartime by soldiers.
-  // 44	Droopy drawers[5]	Rhyme that refers to sagging trousers.[citation needed]
-  // 45	Halfway there	Being halfway towards 90
-  // 46	up to tricks	rhyming
-  // 48	Four Dozen	4 x 12 = 48. Refer to 12 above.
-  // 50	It's a bullseye!	Referring to the darts score.
-  // Snow White	Referring to 5 – 0, it’s off to work we go
-  // 52	Danny La Rue[9]	A reference to drag entertainer Danny La Rue. Also used for other numbers ending in '2' (see '72' below).
-  // Chicken vindaloo[1]	Introduced by Butlins in 2003.[1]
-  // Deck of Cards	Number of cards in a deck
-  // 53	Here comes Herbie	53 is the racing number of Herbie the VW Beetle. Players may reply "beep beep"!
-  // 54	Man at the door	Rhymes with "(Fifty) Four"
-  // 55	All the fives[4]
-  // 56	Shotts Bus[4]	Refers to the former number of the bus from Glasgow to Shotts.
-  // 57	Heinz Varieties[4]	Refers to "Heinz 57", the "57 Varieties" slogan of the H. J. Heinz Company.
-  // 59	The Brighton Line	Quote from The Importance of Being Earnest.
-  // Also, 59 was the starting 2 digits of all original Brighton telephone numbers[citation needed].
-
   // 60	Grandma's getting frisky	Pretty close to a rhyme with 'sixty'
+  // Sixty-One: Bakers Bun
   // 62	Tickety-boo	Rhymes with "(Sixty) Two"
+  // Sixty-Three: Tickle Me
   // 64	Almost retired	A reference to the former British male age of mandatory retirement - specifically being one year away from it.
   // 65	Retirement age, Stop work[2]	A reference to the former male British age of mandatory retirement.
   // 66	Clickety click[5]	Rhymes with "(Sixty) Six"
   // 67	Stairway to Heaven	Coined by Andrew "CIP" Lavelle
   // 68	Pick a Mate	Coined by Edward James Mackey II
   // 69	Anyway up, Meal for Two, A Favourite of mine[2]	A possible reference to the 69 sex position.
+  // Seventy: Three Score and 10
   // 71	Bang on the Drum[2]	Rhymes with "(Seventy) One"
   // 72	Danny La Rue[2]	Rhymes with "(Seventy) Two"
   // 73	Queen Bee. Under The Tree. Lucky 3[10]	Rhymes with "(Seventy) Three"
   // 74	Hit the Floor	Coined by Ann Fitzsimons
+  // Seventy-Five: Strive and Strive
   // 76	Trombones[11]	"Seventy-Six Trombones" is a popular marching song, from the musical The Music Man.
   // Was she worth it?	This refers to the pre-decimal price of a marriage licence in Britain, 7/6d. The players shout back "Every Penny"
   // 77	Two little crutches[11]
   // Sunset Strip	From the 1960s television series "77 Sunset Strip". Usually sung by the players.
   // 78	39 more steps	39 + 39 = 78. Refer to 39 being "39 steps" above.
+  // Seventy-Nine: One More Time
   // 80	Gandhi's Breakfast	"Ate nothing"
   // 81	Fat Lady with a walking stick	The number 8 is supposed to visually represent a lady with ample bosom and hips, while the number 1 is supposed to visually represent a walking stick
+  // Eighty-Two: Straight On Through: referenced to travelling, but also known as a “fat lady with a duck”, which relates to the images the numbers can and have earlier represented.
   // 83	Time for Tea	Rhymes and scans[12]
   // 84	Seven dozen	7 x 12 = 84. Refer to 12 being "a dozen" above
   // 85	Staying alive[13]	Rhymes with "(Eighty) Five"
